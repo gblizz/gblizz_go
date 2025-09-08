f1c7d73bbd346cde9366cd5d113c3d468741f029
G Blizz <42393188+gblizz@users.noreply.github.com>
2025-09-08 05:45:18 -0500

    Got deletetion to work using Zachs pop confirm suggestion.

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,6 +1,6 @@
-import { Button, Checkbox, Input, List, Modal, Popconfirm } from 'antd';
+import { Button, Checkbox, Input, List, Popconfirm } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { createTodo, getTodos, Todo, updateTodo } from '../services/api';
+import { createTodo, deleteTodo, getTodos, Todo, updateTodo } from '../services/api';
 
 
 const TodoList: React.FC = () => {
@@ -17,6 +17,12 @@ const TodoList: React.FC = () => {
     setTitle(''); setDescription('');
   };
 
+const removeTodo = (id: number) => {
+  deleteTodo(id).then(() => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  });
+};
+
   const toggleComplete = (todo: Todo) => {
     updateTodo(todo.id, { ...todo, completed: !todo.completed }).then(() => {
       setTodos(todos.map(t => t.id === todo.id ? { ...t, completed: !t.completed } : t));
@@ -28,7 +34,6 @@ const TodoList: React.FC = () => {
       <Input placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
       <Input placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} style={{ margin: '10px 0' }} />
       <Button type="primary" onClick={addTodo}>Add Todo</Button>
-      <Button onClick={() => Modal.info({ title: 'Test Modal', content: 'This is a test' })}>Test Modal</Button>
       <List
         bordered
         dataSource={todos}
@@ -39,7 +44,7 @@ const TodoList: React.FC = () => {
                 title="Are you sure?" 
                 onConfirm={() => {
                   console.log('Confirmed deletion for ID:', todo.id);
-                  // do the thing
+                  removeTodo(todo.id)
                 }}
               >
                 <Button>Delete</Button>
